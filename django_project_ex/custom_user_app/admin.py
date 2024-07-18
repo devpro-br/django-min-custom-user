4f0f59a5b81e9e6fe2ab2750c71838ff1f907e3f
renzo <renzon@gmail.com>
2024-07-17 22:12:15 -0300

    Implemented first version. Released on PyPI the version 0.2.0

---
+++ django_project_ex/custom_user_app/admin.py
@@ -0,0 +1,5 @@
+from django_min_custom_user.admin import MinUserAdmin
+
+
+class UserAdmin(MinUserAdmin):
+    pass
---
+++ django_project_ex/custom_user_app/models.py
@@ -0,0 +1,5 @@
+from django_min_custom_user.models import MinAbstractUser
+
+
+class User(MinAbstractUser):
+    pass
